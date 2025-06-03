c169d39c0da8147fd97847951da63aaaeeb2f942
plesiosh <plesiosh@gmail.com>
2025-06-02 21:07:09 -0700

    Fix: Prevent duplicate tmux sessions

--- scripts/launch_motors.sh
+++ scripts/launch_motors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tmux new-session -d -s "nav" "bash -c 'python3 src/navigation/motortest.py; exec bash'"
+tmux new-session -d -s "motors" "bash -c 'python3 src/navigation/motortest.py; exec bash'"
 tmux split-window -h "bash -c 'python3 src/navigation/auto_nav.py; exec bash'"
 
-tmux attach-session -t "nav"
+tmux attach-session -t "motors"
