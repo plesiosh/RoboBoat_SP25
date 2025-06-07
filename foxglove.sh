1b88ba1bdee572613e7162460b82f810841dbb2b
plesiosh <plesiosh@gmail.com>
2025-06-06 18:14:02 -0700

    Chore: Allow larger buffer

--- foxglove.sh
+++ foxglove.sh
@@ -1,1 +1,1 @@
-tmux new-session -d -s "foxglove" "bash -c 'ros2 launch foxglove_bridge foxglove_bridge_launch.xml; exec bash'"
+tmux new-session -d -s "foxglove" "bash -c 'ros2 launch foxglove_bridge foxglove_bridge_launch.xml send_buffer_limit:=100000000; exec bash'"
